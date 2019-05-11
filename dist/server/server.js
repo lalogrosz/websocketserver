8f6b8f5a652173b6e2d964886986f335329748e0
Alan Grosz <lalogrosz@gmail.com>
2019-05-11 12:16:43 -0300

    se eliminan archivos

--- dist/server/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-const websocket_server_1 = require("./websocket-server");
-websocket_server_1.default.createServer(app_1.default);
-app_1.default.listen(8000, () => {
-    console.log('HTTP Server started');
-});
-//# sourceMappingURL=server.js.map
